e4c45a92d0a9793668a8c6b64198f9fc46cb4877
thecoder <thecoder@iswleuven.be>
2018-02-16 23:03:56 +0100

    Added new default settings

--- ts/DefaultSettings.ts
+++ ts/DefaultSettings.ts
@@ -5,6 +5,11 @@ export let default_settings = {
         info_file_name: "GitHub-Overview-Info.json",
     },
     game: {
-
+        frame_rate: 60,
+        skyscraper_min_height_difference: 0,
+        skyscraper_max_height_difference: 100,
+        skyscraper_space_inbetween: 200,
+        window_margin_horizontal: 100,
+        crate_margin_right_of_edge: 50,
     }
 };
