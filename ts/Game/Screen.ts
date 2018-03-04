7672f0ca86f731ceb1224f7d9134d717088b98c3
thecoder <thecoder@iswleuven.be>
2018-03-05 00:09:45 +0100

    Set background to city image

--- ts/Game/Screen.ts
+++ ts/Game/Screen.ts
@@ -10,14 +10,18 @@ export default class Screen {
     private canvas:HTMLCanvasElement;
     private visualizableElements:Array<Visualizable>;
     private viewPort:ViewPort;
+    private background_city_sprite:HTMLImageElement;
+    private background_loaded:boolean;
 
     private constructor(context:CanvasRenderingContext2D, canvas:HTMLCanvasElement, visualizableElements:Array<Visualizable>|null=null){
+        this.background_loaded = false;
         this.context = context;
         this.canvas = canvas;
         this.visualizableElements = (visualizableElements===null?new Array<Visualizable>():visualizableElements);
         this.createScreenResizeHandler();
         this.updateScreenSize();
         this.viewPort = new ViewPort(0,0);
+        this.createBackgroundSprite();
     }
 
     public static getInstance(context:CanvasRenderingContext2D|null=null, canvas:HTMLCanvasElement|null=null, visualizableElements:Array<Visualizable>|null=null):Screen{
@@ -35,13 +39,37 @@ export default class Screen {
     }
 
     draw(elements:Array<Visualizable>):void{
+        if(!this.background_loaded) return;
+        
+        //DRAw IMAGE
+        let modifierToScreen = this.getWidth() / this.background_city_sprite.width;
+        let modifierToImage = this.background_city_sprite.width / this.getWidth();
+        let cx2 = this.background_city_sprite.width;
+        let cy2 = this.background_city_sprite.height > this.getHeight() * modifierToImage ? this.getHeight() * modifierToImage : this.background_city_sprite.height;
+        let x = 0;
+        let y = this.getHeight() - this.background_city_sprite.height * modifierToScreen < 0 ? 0 : this.getHeight() - this.background_city_sprite.height * modifierToScreen;
+        let w = this.getWidth();
+        let h = this.getHeight() - y;
+
         this.context.beginPath();
-        this.context.rect(0,0,this.canvas.width, this.canvas.height);
-        this.context.fillStyle = "lightblue";
-        this.context.fill();
+        this.context.drawImage(this.background_city_sprite, 0, 0, cx2, cy2, x, y, w, h);
+
+        if(y > 0){
+            //STRETCH TOP PART OF SKY UP FOR INFINITE SKY IF THERE IS EMPTY SPACE
+            let stretch_part_height = 10; //HEIGHT OF STRETCH PART IN PX
+            this.context.beginPath();
+            this.context.drawImage(this.background_city_sprite, 0, 0, this.background_city_sprite.width, stretch_part_height, 0, 0, this.getWidth(), y+stretch_part_height);
+        }
+
         for(let i=0;i<elements.length;i++){
             elements[i].draw(this.context);
         }
+    }
+
+    private createBackgroundSprite(){
+        this.background_city_sprite = new Image();
+        this.background_city_sprite.src = "Images/backgroundCity.png";
+        this.background_city_sprite.onload = (()=>{ this.background_loaded = true; }).bind(this);
     }
 
     updateVisualizableElements(visualizableElements:Array<Visualizable>){
